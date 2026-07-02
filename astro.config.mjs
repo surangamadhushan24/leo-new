48b74a10c3a4df9f7d7c5e68743f6a0cea966c85
Leo Club Developer <dev@leo-club.local>
2026-07-02 20:45:15 +0530

    update urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bgleo.netlify.app/',
   // No base path needed for Netlify root domain
   output: 'static',
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx(), sitemap()]
 });
